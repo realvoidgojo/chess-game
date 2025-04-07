adac9e0ff165bc5eeb24eca8c8228370c557c1e7
realvoidgojo <dir.harishsivaraman@gmail.com>
2025-04-07 19:53:09 +0530

    chessboard structure made using CSS

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Chessboard } from "./components/Chessboard";
+function App() {
+  return (
+    <div id="app">
+      <Chessboard />
+    </div>
+  );
+}
+
+export default App;
